b859fd20363c095087f758a41376d334b8a0494c
Silvia Avelar <150369573+SilviaAvelar@users.noreply.github.com>
2024-01-03 21:07:29 +0000

    ex004BikeEletrica

---
+++ objetos.js
@@ -0,0 +1,20 @@
+// Criar um objeto
+// Uma Bike Sustentável
+let Bike = {
+    Nome: 'Bike Free',
+    Marca: 'Samsung',
+    Cor: 'Verde',
+    Ano: '2023',
+    Combustível: 'energia',
+    Econômica: 'Sim',
+    Preco: '500'
+
+}
+
+console.log(Bike)
+
+console.log('Vou comprar uma ' + Bike.Nome + ' da ' 
++ Bike.Marca + ' na cor ' + Bike.Cor + ' Ano '
++ Bike.Ano + ' na bagatela de ' + Bike.Preco + ' euros!',
+'Movida a ' + Bike.Combustível + ' e ' + Bike.Econômica +
+' muito econômica! ')
